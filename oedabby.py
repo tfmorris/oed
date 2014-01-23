b4fc75baf22e6494889acdba62643c24f4b76252
Tom Morris <tfmorris@gmail.com>
2014-01-23 14:05:32 -0500

    Auto download & text block merge 1st pass
    Make more self contained by downloading file automatically from Internet
    Archive.  Add 1st pass of a block merge algorithm to reassemble block
    fragments.  Work in progress from Sept. 2013.

--- oedabby.py
+++ oedabby.py
@@ -1,9 +1,11 @@
+import gzip
 import lxml.etree as ET
+import os
 import requests
 from xml.etree import cElementTree
 import zlib
 
-PAGELIMIT=30
+PAGELIMIT=300
 SIZE=5*1024*1024
 XMLTEMPLATE = 'output/oed-vol1_p%d.xml'
 HTMLTEMPLATE = 'output/oed-vol1_p%d.html'
@@ -11,45 +13,67 @@ HEADER = ['<?xml version="1.0" encoding="UTF-8"?>',
 #    '<document version="1.0" producer="LuraDocument XML Exporter for ABBYY FineReader" pagesCount="1"',
 #    'xmlns="http://www.abbyy.com/FineReader_xml/FineReader6-schema-v1.xml" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.abbyy.com/FineReader_xml/FineReader6-schema-v1.xml http://www.abbyy.com/FineReader_xml/FineReader6-schema-v1.xml">',
     ]
+FILENAME = 'https://ia600401.us.archive.org/7/items/oed01arch/oed01arch_abbyy.gz'
 
 
 xslt = ET.parse('abbyy2hocr.xsl')
 transform = ET.XSLT(xslt)
 
-f = 'https://ia600401.us.archive.org/7/items/oed01arch/oed01arch_abbyy.gz'
-r = requests.get(f, stream=True)
-
-zd = zlib.decompressobj(16+zlib.MAX_WBITS)
-buf = r.raw.read(SIZE)
-
-pagenum = 0
-linenum = 0
-lines = zd.decompress(buf).split('\n')
-while linenum < len(lines):
-    linenum += 1
-    line = lines[linenum]
-    if line.startswith('<page'):
-        pagenum += 1
-        if pagenum > PAGELIMIT:
-            break
-        xml = list(HEADER)
-        while not line.startswith('</page'):
-            xml.append(line)
-            linenum += 1
-            line = lines[linenum]
-        xml.append(line)
-#        xml.append('</document>')
+def mergeblocks(dom):
+        '''
+        Sort text blocks by column and merge those where the columns
+        were split up into multiple blocks.  
+        '''
+        blocks = dom.findall(".//div[@class='ocr_carea column']")
+        print 'Found %d blocks' % len(blocks)
+        cols=[]
+        for i in range(3):
+                cols[i]=[]
+        pl = pt = pr = pb = 1500
+        for block in blocks:
+                bbox = block.attrib['title'].split('bbox')[1]
+                l,t,r,b = map(int,bbox.strip().split(' '))
+                if l < pl:
+                        pl = l
+                if t < pt:
+                        pt = t
+                if r > pr:
+                        pr = r
+                if b > pb:
+                        pb = b
+                # Nominal column width is ~720-750 pixels
+                w = r-l
+                # Full column height is ~3045-3115 pixels
+                h = b-t
+                # Nominal column centers 415, 1170, 1920 or 725, 1480, 2230
+                # (ie 300 px offset on facing pages)
+                c = l+(w)/2
+                print "%d\t%d\t%d\t%d\t%d\t%d\t%d" % (l,r,t,b,c,w,h)
+                # Sort by column & then by Y position before merging
+                # ** need to watch for overlapping bboxes ie bad segmentation**
+                # make sure candidates to be merged are the same shape & adjacent
+                if (abs(lastcenter - c) < 100):
+                        print("merging")
+                        lastblock.extend(block.findall("*"))
+                        # TODO: update bounding box?
+                        block.find("..").remove(block)
+                        block.clear
+                else:
+                        lastcenter = c
+                        lastblock = block
+        pw = pr - pl
+        ph = pb - pt
+        print "Page: %d\t%d\t%d\t%d" % (pl,pt,pw,ph)
+        blocks = dom.findall(".//div[@class='ocr_carea column']")
+        print 'Finally remaining %d blocks' % len(blocks)
         
-# Our extracted XML file if it's interesting for debugging
-        with file(XMLTEMPLATE % pagenum, 'w') as of:
-            of.write('\n'.join(xml))
-            
-        dom = ET.fromstring('\n'.join(xml))
+def postprocess(dom):
+        '''
+        Post-process our HTML in an attempt to improve it
+        '''
+        mergeblocks(dom)
 
-        # Transform to hOCR
-        newdom = transform(dom)
-
-        # post-process HTML
+        # merge multiple blocks in a column
 
         # strip headwords
 
@@ -57,7 +81,67 @@ while linenum < len(lines):
 
         # apply semantic markup to entries
 
-        print 'Writing page %d - %d XML lines processed' % (pagenum,linenum)
-        with file(HTMLTEMPLATE % pagenum, 'w') as of:
-            of.write(ET.tostring(newdom, pretty_print=True))
-        
+        # concatenate hyphenated words
+
+        # add next/previous page links
+        return dom
+
+def download(remote,local):
+    print 'Downloading %s to %s' % (remote, local)
+    r = requests.get(remote, stream=True)
+    if r.status_code == 200:
+        with open(local, 'wb') as f:
+            for chunk in r.iter_content():
+                f.write(chunk)
+    else:
+        print 'Download failed with status code %d' % r.status_code
+
+def processfile(filename):
+    localfile = 'input/'+filename.split('/')[-1]
+    if not os.path.exists(localfile):
+        download(filename,localfile)
+
+    # Old code to just read a few MB over the network & decompress it
+    #    r = requests.get(f, stream=True)
+    #    buf = r.raw.read(SIZE)
+    #    zd = zlib.decompressobj(16+zlib.MAX_WBITS)
+    #    lines = zd.decompress(buf).split('\n')
+
+    print 'Opening %s' % localfile
+    pagenum = 0
+    linenum = 0
+    with gzip.open(localfile, 'rb') as f:
+        for line in f:
+                linenum += 1
+                if line.startswith('<page'):
+                    pagenum += 1
+                    if pagenum > PAGELIMIT:
+                        break
+                    xml = list(HEADER)
+                    while not line.startswith('</page'):
+                        xml.append(line)
+                        linenum += 1
+                        line = f.next()
+                    xml.append(line)
+            #        xml.append('</document>')
+                    
+            # Our extracted XML file if it's interesting for debugging
+            #        with file(XMLTEMPLATE % pagenum, 'w') as of:
+            #            of.write('\n'.join(xml))
+
+                    dom = ET.fromstring('\n'.join(xml))
+
+                    # Transform to hOCR
+                    newdom = transform(dom)
+
+                    newdom = postprocess(newdom)
+
+
+                    print 'Writing page %d - %d XML lines processed' % (pagenum,linenum)
+                    with file(HTMLTEMPLATE % pagenum, 'w') as of:
+                        of.write(ET.tostring(newdom, pretty_print=True))
+
+def main():
+    processfile(FILENAME)
+
+main()
